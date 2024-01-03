603ef45b3bc3fba8e8df5a54f5fb491e40d44504
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-01-03 13:48:17 +0600

    get contacts added

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://relate-hub:92i3X1mDyecTHAPm@cluster0.pdzlhd7.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("relate-hub");
+    const contactCollection = database.collection("contacts");
+    // const userCollection = client.db("usersDB").collection("users");
+
+    app.post("/contacts", async (req, res) => {
+      const data = req.body;
+      const result = await contactCollection.insertOne(data);
+      res.send(result);
+      console.log(result);
+    });
+  app.get("/contacts", async(req,res)=>{
+    const result = await contactCollection.find().toArray();
+    res.send(result);
+  })
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("SIMPLE CRUD IS RUNNING WELL");
+});
+
+app.listen(port, () => {
+  console.log(`simple crud is running in port :${port}`);
+});
