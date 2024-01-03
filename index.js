3805e970f9280c728dbfd84a9447ed16b525ff9b
AlaminHasanPro <alaminhasanpro@gmail.com>
2024-01-03 21:36:28 +0600

    env safe done

--- index.js
+++ index.js
@@ -3,16 +3,15 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
-
+require("dotenv").config();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri =
-  "mongodb+srv://relate-hub:92i3X1mDyecTHAPm@cluster0.pdzlhd7.mongodb.net/?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pdzlhd7.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
